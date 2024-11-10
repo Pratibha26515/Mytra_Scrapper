acd5fb4fc04d7fc62d1f768e4cfa47f0c9e41b9f
Pratibha <pratibhauniyal2004@gmail.com>
2024-11-10 17:36:26 +0530

    Initial commit with project files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Mynscrapper',
+    version='1.0',
+    author='Pratibha',
+    author_email='pratibhauniyal2004@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
